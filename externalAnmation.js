e799869e702a372e1b14eb7bba95820685c3332f
NishchalPersonal <bhardwajnishchal1999@gmail.com>
2025-03-02 20:15:16 -0500

    added css start animation in the background

---
+++ externalAnmation.js
@@ -0,0 +1,28 @@
+function createAnimation() {
+    
+    const documentBody = document.body;
+    var counter = 0;
+    const pairList = [["top", "left"], ["top", "right"], ["bottom", "left"], ["bottom", "right"]]
+
+    for(var x =0; x < pairList.length; x++){
+        var getPair = pairList[x];
+        const createStarDiv = document.createElement("div");
+        createStarDiv.classList.add("star");
+
+        createStarDiv.style[getPair[0]] = (Math.floor(Math.random() * 95) + 5)+'%';
+        createStarDiv.style[getPair[1]] =  ((Math.floor(Math.random() * 95) + 5))+'%'
+        counter = counter + 1;
+        documentBody.appendChild(createStarDiv)
+        if(x == pairList.length -1){
+            x = 0;
+        }
+        if(counter > 12){
+            break;
+        }
+    }
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    createAnimation();
+});
+
